6e2ff776d8735fbc3887a89d51622f53b9aa473d
eugene.sky <skytech4dotnet@gmail.com>
2021-01-19 17:41:39 -0600

    pageable filterable grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { GridDataResult, DataStateChangeEvent } from '@progress/kendo-angular-grid';
+import { process, State } from '@progress/kendo-data-query';
+import { products } from './products';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'k-grid';
+
+  public state: State = {
+    skip: 0,
+    take: 10,
+
+    // Initial filter descriptor
+    filter: {
+      logic: 'and',
+      filters: [{ field: 'ProductName', operator: 'contains', value: 'Chef' }]
+    }
+};
+
+public gridData: GridDataResult = process(products, this.state);
+
+public dataStateChange(state: DataStateChangeEvent): void {
+    this.state = state;
+    this.gridData = process(products, this.state);
+}  
 }
